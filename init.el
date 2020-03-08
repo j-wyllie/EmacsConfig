19656cc8ff15d7f7f4f5f20654f972796010c726
j-wyllie <joshuawyllie0@gmail.com>
2020-03-08 16:41:07 +1300

    Add MELPA repository in order to install the haskell-mode package

--- init.el
+++ init.el
@@ -1,4 +1,20 @@
 ;; use evil mode
+
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
@@ -66,7 +82,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (evil)))
+ '(package-selected-packages (quote (haskell-mode evil)))
  '(tool-bar-mode nil))
 
 (custom-set-faces
